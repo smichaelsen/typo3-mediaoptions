8a280896892ebb1e592b168864ce671ae532c7ff
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-09-23 12:43:48 +0200

    [FEATURE] Custom YouTube renderer

---
+++ Classes/Resource/Rendering/YouTubeRenderer.php
@@ -0,0 +1,168 @@
+<?php
+namespace Smichaelsen\Mediaoptions\Resource\Rendering;
+
+use Smichaelsen\ShortcutParams\TypoScriptFrontendController;
+use TYPO3\CMS\Core\Resource\FileInterface;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Service\FlexFormService;
+
+class YouTubeRenderer extends \TYPO3\CMS\Core\Resource\Rendering\YouTubeRenderer
+{
+
+    /**
+     * Returns the priority of the renderer
+     * @return int
+     */
+    public function getPriority()
+    {
+        return 5;
+    }
+
+    /**
+     * Render for given File(Reference) html output
+     *
+     * @param FileInterface $file
+     * @param int|string $width TYPO3 known format; examples: 220, 200m or 200c
+     * @param int|string $height TYPO3 known format; examples: 220, 200m or 200c
+     * @param array $options
+     * @param bool $usedPathsRelativeToCurrentScript See $file->getPublicUrl()
+     * @return string
+     */
+    public function render(FileInterface $file, $width, $height, array $options = null, $usedPathsRelativeToCurrentScript = false)
+    {
+
+        if ($file instanceof FileReference) {
+            $orgFile = $file->getOriginalFile();
+        } else {
+            $orgFile = $file;
+        }
+        $videoId = $this->getOnlineMediaHelper($file)->getOnlineMediaId($orgFile);
+
+        if (empty($options['additionalConfig']['mediaoptions'])) {
+            // legacy options rendering
+            $embedUrlParameterString = $this->getLegacyEmbedUrlParameterString($options);
+        } else {
+            $embedUrlParameters = $this->getEmbedUrlParameters($options['additionalConfig']['mediaoptions'], $videoId);
+            // @todo: insert hook to modify $embedUrlParameters
+            $embedUrlParameterString = http_build_query($embedUrlParameters);
+        }
+
+        $src = sprintf(
+            '//www.youtube%s.com/embed/%s?%s',
+            !empty($options['no-cookie']) ? '-nocookie' : '',
+            $videoId,
+            $embedUrlParameterString
+        );
+
+        $attributes = ['allowfullscreen'];
+        if ((int)$width > 0) {
+            $attributes[] = 'width="' . (int)$width . '"';
+        }
+        if ((int)$height > 0) {
+            $attributes[] = 'height="' . (int)$height . '"';
+        }
+        if (is_object($GLOBALS['TSFE']) && $GLOBALS['TSFE']->config['config']['doctype'] !== 'html5') {
+            $attributes[] = 'frameborder="0"';
+        }
+        foreach (['class', 'dir', 'id', 'lang', 'style', 'title', 'accesskey', 'tabindex', 'onclick', 'poster', 'preload'] as $key) {
+            if (!empty($options[$key])) {
+                $attributes[] = $key . '="' . htmlspecialchars($options[$key]) . '"';
+            }
+        }
+
+        return sprintf(
+            '<iframe src="%s"%s></iframe>',
+            $src,
+            empty($attributes) ? '' : ' ' . implode(' ', $attributes)
+        );
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    protected function getLegacyEmbedUrlParameterString($options)
+    {
+        $urlParams = ['autohide=1'];
+        if (!isset($options['controls']) || !empty($options['controls'])) {
+            $urlParams[] = 'controls=2';
+        }
+        if (!empty($options['autoplay'])) {
+            $urlParams[] = 'autoplay=1';
+        }
+        if (!empty($options['loop'])) {
+            $urlParams[] = 'loop=1';
+        }
+        if (!isset($options['enablejsapi']) || !empty($options['enablejsapi'])) {
+            $urlParams[] = 'enablejsapi=1&amp;origin=' . GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL');
+        }
+        $urlParams[] = 'showinfo=' . (int)!empty($options['showinfo']);
+        return join('&amp;', $urlParams);
+    }
+
+    /**
+     * @param string $mediaoptions Flexform string
+     * @param string $videoId
+     * @return array
+     */
+    protected function getEmbedUrlParameters($mediaoptions, $videoId)
+    {
+        $embedUrlParameters = [];
+        $mediaoptionsFlexFormData = GeneralUtility::makeInstance(ObjectManager::class)->get(FlexFormService::class)->convertFlexFormContentToArray($mediaoptions);
+        if ((int)$mediaoptionsFlexFormData['autoplay'] === 1) {
+            $embedUrlParameters['autoplay'] = 1;
+        }
+        if ((int)$mediaoptionsFlexFormData['ccLoadPolicy'] === 1) {
+            $embedUrlParameters['cc_load_policy'] = 1;
+        }
+        if ((int)$mediaoptionsFlexFormData['enablejsapi'] === 1) {
+            $embedUrlParameters['enablejsapi'] = 1;
+        }
+        if (!empty($mediaoptionsFlexFormData['end'])) {
+            $embedUrlParameters['end'] = (int)$mediaoptionsFlexFormData['end'];
+        }
+        if ((int)$mediaoptionsFlexFormData['hideAnnotation'] === 1) {
+            $embedUrlParameters['iv_load_policy'] = 3;
+        }
+        if ((int)$mediaoptionsFlexFormData['hideControls'] === 1) {
+            $embedUrlParameters['controls'] = 0;
+        }
+        if ((int)$mediaoptionsFlexFormData['loop'] === 1) {
+            $embedUrlParameters['loop'] = 1;
+            $embedUrlParameters['playlist'] = $videoId;
+        }
+        if ((int)$mediaoptionsFlexFormData['modestBranding'] === 1) {
+            $embedUrlParameters['modestbranding'] = 1;
+        }
+        if ((int)$mediaoptionsFlexFormData['passLanguageAlong'] === 1) {
+            $languageIsoCode = $this->getTypoScriptFrontendController()->sys_language_isocode;
+            if (!empty($languageIsoCode)) {
+                $embedUrlParameters['hl'] = $languageIsoCode;
+            }
+        }
+        if ((int)$mediaoptionsFlexFormData['preventFullScreen'] === 1) {
+            $embedUrlParameters['fs'] = 0;
+        }
+        if ((int)$mediaoptionsFlexFormData['relatedVideos'] === 0) {
+            $embedUrlParameters['rel'] = 0;
+        }
+        if (!empty($mediaoptionsFlexFormData['start'])) {
+            $embedUrlParameters['start'] = (int)$mediaoptionsFlexFormData['start'];
+        }
+        if ($mediaoptionsFlexFormData['color'] === 'white') {
+            $embedUrlParameters['color'] = 'white';
+        }
+        return $embedUrlParameters;
+    }
+
+    /**
+     * @return TypoScriptFrontendController
+     */
+    protected function getTypoScriptFrontendController()
+    {
+        return $GLOBALS['TSFE'];
+    }
+
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,8 @@
 <?php
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['getFlexFormDSClass'][] = \Smichaelsen\Mediaoptions\Hooks\GetFlexformDataStructureHook::class;
+
+/** @var \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry $rendererRegistry */
+$rendererRegistry = \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::getInstance();
+$rendererRegistry->registerRendererClass(\Smichaelsen\Mediaoptions\Resource\Rendering\YouTubeRenderer::class);
+unset($rendererRegistry);
