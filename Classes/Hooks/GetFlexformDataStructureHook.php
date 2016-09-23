b86923ba41110aeaf47b0160ec981d3390211c1e
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-09-23 09:52:50 +0200

    [FEATURE] Display flexform in sys_file_reference depending on the file type
    Includes Flexforms for YouTube and Vimeo

--- Classes/Hooks/GetFlexformDataStructureHook.php
+++ Classes/Hooks/GetFlexformDataStructureHook.php
@@ -1,6 +1,9 @@
 <?php
 namespace Smichaelsen\Mediaoptions\Hooks;
 
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
+
 class GetFlexformDataStructureHook
 {
 
@@ -14,8 +17,38 @@ class GetFlexformDataStructureHook
     public function getFlexFormDS_postProcessDS(&$dataStructArray, $conf, $row, $table, $fieldName)
     {
         if (strpos($conf['ds_pointerField'], ':') !== false) {
-            // do it!
+            list($localField, $foreignField) = explode(':', $conf['ds_pointerField']);
+            $foreignTable = $this->getForeignTable($table, $localField);
+            $foreignUid = $row[$localField];
+            if (!MathUtility::canBeInterpretedAsInteger($foreignUid)) {
+                list($foreignUid) = explode('|', $foreignUid);
+                $foreignUid = str_replace($foreignTable . '_', '', $foreignUid);
+            }
+            $foreignRecord = BackendUtility::getRecord($foreignTable, $foreignUid, $foreignField);
+            if (!is_array($foreignRecord)) {
+                return;
+            }
+            $foreignConf = $conf;
+            $foreignConf['ds_pointerField'] = $foreignField;
+            $dataStructArray = BackendUtility::getFlexFormDS($foreignConf, $foreignRecord, $foreignTable, $foreignField);
         }
+    }
+
+    /**
+     * @param $table
+     * @param $localField
+     * @return string
+     */
+    protected function getForeignTable($table, $localField)
+    {
+        $fieldConfig = $GLOBALS['TCA'][$table]['columns'][$localField]['config'];
+        if ($fieldConfig['type'] === 'group') {
+            $allowedTables = explode(',', $fieldConfig['allowed']);
+            return $allowedTables[0];
+        } elseif ($fieldConfig['type'] === 'select') {
+            return $fieldConfig['foreign_table'];
+        }
+        return '';
     }
 
 }
--- Configuration/TCA/Overrides/sys_file_reference.php
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -1,11 +1,13 @@
 <?php
 
+unset($GLOBALS['TCA']['sys_file_reference']['columns']['autoplay']);
 $GLOBALS['TCA']['sys_file_reference']['columns']['mediaoptions'] = [
     'label' => 'Options',
     'config' => [
         'type' => 'flex',
         'ds_pointerField' => 'uid_local:mime_type',
         'ds' => [
+            'video/vimeo' => 'FILE:EXT:mediaoptions/Configuration/FlexForms/Vimeo.xml',
             'video/youtube' => 'FILE:EXT:mediaoptions/Configuration/FlexForms/YouTube.xml',
         ],
     ],
